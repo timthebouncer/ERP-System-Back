0713c6ea646916dcc9c1979b61f53d8a700ac9a5
timthebouncer <he01367637us@gmail.com>
2020-10-22 14:24:03 +0800

    客戶管理 商品管理

--- src/views/Customer/index.vue
+++ src/views/Customer/index.vue
@@ -1,11 +1,408 @@
 <template>
-    <div>
-        客戶管理
+    <div class="container">
+        <div class="action">
+            <div class="addM">
+                <a-button class="button1" @click="showModal">新增+</a-button>
+                <a-modal v-model="visible" :title=changeTitle width="1000px">
+                    <div class="modal-body">
+                        <a-form-model :model="list"  layout="horizontal">
+                            <div class="firstPart">
+                                <div class="firstPart-item">
+                                    <a-form-model-item class="custom-form-item" label="客戶編號">
+                                        <div style="width: 100px">{{123}}</div>
+                                    </a-form-model-item>
+                                    <a-form-model-item class="custom-form-item" label="客戶類別">
+                                        <a-select  v-model="list.classes" placeholder="請選擇">
+                                            <a-select-option value="" v-for="item in classify">
+                                                {{item.id}}
+                                            </a-select-option>
+                                        </a-select>
+                                    </a-form-model-item>
+                                    <a-form-model-item class="custom-form-item" label="客戶姓名">
+                                        <a-input v-model="list.name" placeholder="請輸入" />
+                                    </a-form-model-item>
+                                </div>
+                                <div class="firstPart-item">
+                                    <a-form-model-item class="custom-form-item" label="電話">
+                                        <a-input v-model="list.cellphone" placeholder="請輸入" />
+                                    </a-form-model-item>
+                                    <a-form-model-item class="custom-form-item" label="email">
+                                        <a-input v-model="list.email" placeholder="請輸入" />
+                                    </a-form-model-item>
+                                </div>
+                                <a-form-model-item class="custom-form-item address" label="地址">
+                                    <div>
+                                        <a-input v-model="list.postcode" placeholder="郵遞區號" />
+                                    </div>
+                                    <div>
+                                        <a-input v-model="list.address" placeholder="請輸入" />
+                                    </div>
+                                </a-form-model-item>
+
+
+                                <a-form-model-item label="備註">
+                                    <div>
+                                        <a-input placeholder="請輸入" />
+                                    </div>
+                                </a-form-model-item>
+                            </div>
+                            <div class="secondPart">
+                                <div class="secondPart-item">
+                                    <a-form-model-item class="custom-form-item" label="公司名稱">
+                                        <a-input v-model="list.company" placeholder="請輸入" style="width: 300px" />
+                                    </a-form-model-item>
+
+                                    <a-form-model-item class="custom-form-item" label="統一編號">
+                                        <a-input v-model="list.vatNumber" placeholder="請輸入" />
+                                    </a-form-model-item>
+
+                                    <a-form-model-item label="聯絡人">
+                                        <a-input v-model="list.contactPerson" placeholder="請輸入" />
+                                    </a-form-model-item>
+
+                                    <a-form-model-item label="公司電話">
+                                        <a-input v-model="list.tel" placeholder="請輸入" />
+                                    </a-form-model-item>
+
+                                    <a-form-model-item label="傳真">
+                                        <a-input v-model="list.fax" placeholder="請輸入" />
+                                    </a-form-model-item>
+
+                                    <a-form-model-item label="公司email">
+                                        <a-input v-model="list.companyEmail" placeholder="請輸入" />
+                                    </a-form-model-item>
+                                </div>
+                                <a-form-model-item class="custom-form-item address" label="公司地址">
+                                    <div style="width: 10%">
+                                        <a-input v-model="list.companyPostcode" placeholder="郵遞區號" />
+                                    </div>
+                                    <div>
+                                        <a-input v-model="list.companyAddress" placeholder="請輸入" />
+                                    </div>
+                                </a-form-model-item>
+                            </div>
+                        </a-form-model>
+                    </div>
+                    <template slot="footer">
+                        <a-button key="submit" type="primary" :loading="loading" @click="handleOk">
+                            儲存
+                        </a-button>
+                        <a-button key="back" @click="handleCancel">
+                            取消
+                        </a-button>
+                    </template>
+                </a-modal>
+            </div>
+
+            <div class="search">
+                <a-input-search v-model="search" placeholder="搜尋內容" enter-button @click="onSearch" />
+            </div>
+        </div>
+        <div class="itemMenu">
+            <a-table :columns="columns" :data-source="tableData" bordered :pagination="false">
+                <template
+                        v-for="col in ['order','id','classify','name','cellPhone','company','vatNumber','contactPerson',
+                        'tel']"
+                        :slot="col"
+                        slot-scope="text, record, index"
+                >
+                    <div :key="col">
+                        <template v-if="col === 'order'">
+                            {{index+1}}
+                        </template>
+                        <template>
+                            {{ text }}
+                        </template>
+                    </div>
+                </template>
+                <template slot="operation" slot-scope="text, record">
+                    <a-button size="small" @click="editHandler(record)">編輯</a-button>
+                    <a-popconfirm
+                            title="確定要刪除嗎?"
+                            @confirm="() => onDelete(record.id)"
+                    >
+                        <a-button size="small">刪除</a-button>
+                    </a-popconfirm>
+                </template>
+            </a-table>
+        </div>
+        <!--分頁-->
+        <a-pagination class="pagination"
+                      v-model="current"
+                      :page-size-options="pageSizeOptions"
+                      :total="total"
+                      show-size-changer
+                      :page-size="pageSize"
+                      @showSizeChange="onShowSizeChange"
+        >
+            <template slot="buildOptionText" slot-scope="props">
+                <span v-if="props.value !== '50'">{{ props.value }}筆/頁</span>
+                <span v-if="props.value === '50'">全部</span>
+            </template>
+        </a-pagination>
+
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
+        name:"Customer",
+        data(){
+            return{
+                lc:{span:6},
+                wc:{span:18},
+                loading: false,
+                visible:false,
+                changeTitle:"",
+                search:"",
+                tableData: [],
+                classify: [],
+                list: {
+                    classes: '',
+                    name: '',
+                    cellphone: '',
+                    email: '',
+                    address: '',
+                    postcode: '',
+                    company: '',
+                    vatNumber: '',
+                    contactPerson: '',
+                    tel: '',
+                    companyEmail: '',
+                    fax: '',
+                    companyPostcode: '',
+                    companyAddress: '',
+                },
+                columns: [
+                    {
+                        title: '序',
+                        dataIndex: 'order',
+                        width: '2%',
+                        align:"center",
+                        scopedSlots: { customRender: 'order' },
+                    },
+                    {
+                        title: '客戶編號',
+                        dataIndex: 'id',
+                        width: '10%',
+                        align:"center",
+                        scopedSlots: { customRender: 'id' },
+                    },
+                    {
+                        title: '客戶類別',
+                        dataIndex: 'classify',
+                        width: '15%',
+                        align:"center",
+                        scopedSlots: { customRender: 'classify' },
+                    },
+                    {
+                        title: '客戶姓名',
+                        dataIndex: 'name',
+                        width: '15%',
+                        align:"center",
+                        scopedSlots: { customRender: 'name' },
+                    },
+                    {
+                        title: '電話',
+                        dataIndex: 'cellPhone',
+                        width: '5%',
+                        align:"center",
+                        scopedSlots: { customRender: 'cellPhone' },
+                    },
+                    {
+                        title: '公司名稱',
+                        dataIndex: 'company',
+                        width: '10%',
+                        align:"center",
+                        scopedSlots: { customRender: 'company' },
+                    },
+                    {
+                        title: '統一編號',
+                        dataIndex: 'vatNumber',
+                        width: '10%',
+                        align:"center",
+                        scopedSlots: { customRender: 'vatNumber' },
+                    },
+                    {
+                        title: '聯絡人',
+                        dataIndex: 'contactPerson',
+                        width: '10%',
+                        align:"center",
+                        scopedSlots: { customRender: 'contactPerson' },
+                    },
+                    {
+                        title: '公司電話',
+                        dataIndex: 'tel',
+                        width: '5%',
+                        align:"center",
+                        scopedSlots: { customRender: 'tel' },
+                    },
+                    {
+                        title: 'operation',
+                        dataIndex: 'operation',
+                        width:'10%',
+                        align:"center",
+                        scopedSlots: { customRender: 'operation' },
+                    },
+                ],
+                pageSizeOptions: ['10', '20', '30'],
+                current:1,
+                pageSize: 10,
+                total: 30,
+            }
+        },
+        created() {
+            this.getList()
 
+            axios.get('/erp/client/classes')
+                .then((res) => {
+                    this.classify = res.data
+                }).catch((err) => {
+                console.log(err)
+            })
+        },
+        methods:{
+            getList() {
+                axios.get('/erp/client/clients?searchKeyword=')
+                    .then((res) => {
+                        this.tableData = res.data
+                    })
+            },
+            showModal() {
+                this.visible = true;
+                this.changeTitle = "新增客戶"
+            },
+            handleOk() {
+                if (this.changeTitle === '新增客戶') {
+                    axios.post('/erp/client/addClient', {
+                        id: "",
+                        classesId: this.list.classes,
+                        name: this.list.name,
+                        cellphone: this.list.cellphone,
+                        company: this.list.company,
+                        vatNumber: this.list.vatNumber,
+                        contactPerson: this.list.contactPerson,
+                        tel: this.list.tel,
+                        address: this.list.address,
+                        fax: this.list.fax
+                    }).then(() => {
+                        this.getList()
+                    }).catch((err) => {
+                        console.log(err)
+                    })
+                    this.visible = false
+                } else {
+                    axios.put('/erp/client/updateClient', {
+                        id:this.track,
+                        classesId: this.list.classes.id,
+                        name: this.list.name,
+                        cellphone: this.list.cellphone,
+                        company: this.list.company,
+                        vatNumber: this.list.vatNumber,
+                        contactPerson: this.list.contactPerson,
+                        tel: this.list.tel,
+                        address: this.list.address,
+                        fax: this.list.fax
+                    }).then(() => {
+                        this.getList()
+                    }).catch((err) => {
+                        console.log(err)
+                    })
+                    this.visible = false
+                }
+            },
+            handleCancel(e) {
+                this.visible = false;
+            },
+            editHandler(record){
+                this.track = record.id
+                this.changeTitle = "編輯客戶"
+                axios.get('/erp/client/' + record.id)
+                    .then((res) => {
+                        if (res.data !== "") {
+                            this.list = res.data
+                            this.visible = true
+                        } else {
+                            alert("123")
+                        }
+                    }).catch((err) => {
+                    console.log(err)
+                })
+            },
+            onDelete(){
+                axios.delete('/erp/client/removeClient/'+ id)
+                    .then(()=>{
+                        this.getList()
+                    })
+            },
+            onShowSizeChange(current, pageSize) {
+                this.pageSize = pageSize;
+            },
+            onSearch(){
+
+            }
+        }
     }
 </script>
+
+<style scoped lang="scss">
+    .button1{
+        background-color: #1ebf1e;
+        color: #fcfcfc;
+        font-weight: bold;
+        font-size: large;
+    }
+    .action{
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 15px;
+    }
+    .search{
+        width: 300px;
+    }
+    .firstPart{
+
+    }
+    .firstPart-item{
+        display: flex;
+        justify-content: flex-start;
+
+    }
+    .firstPart-item > div {
+        flex: 1;
+    }
+    .firstPart-item > .aaa {
+        display: flex;
+        line-height: 2;
+        font-size:medium;
+        flex: initial;
+    }
+
+    /*.secondPart{*/
+    /*    margin-top: 10px;*/
+    /*    display: flex;*/
+    /*    flex-wrap: wrap;*/
+    /*    justify-content: space-between;*/
+    /*}*/
+    .pagination{
+        display: flex;
+        justify-content: flex-end;
+        margin-top: 20px;
+    }
+    .address{
+        /*display: flex;*/
+        /*justify-content: flex-start;*/
+    }
+    /*.address > div{*/
+    /*    flex: 1*/
+    /*}*/
+
+    .address::v-deep .ant-form-item {
+        /*display: none;*/
+    }
+    .address::v-deep .ant-form-item .ant-form-item-label {}
+    .address::v-deep .ant-form-item .ant-form-item-control {}
+
+
+</style>
--- src/views/Inventory/index.vue
+++ src/views/Inventory/index.vue
@@ -1,15 +1,359 @@
 <template>
-  <div>
-    庫存管理
+  <div class="container">
+    <div class="action">
+      <div class="addM">
+        <a-button class="button1" @click="showModal">新增+</a-button>
+        <a-modal v-model="visible" :title=changeTitle width="1000px">
+          <div class="modal-body">
+            <a-input-group size="default">
+              <a-row :gutter="20">
+                <div class="allPart">
+                  <div class="firstPart">
+                    <div>
+                      商品名稱
+                    </div>
+                    <a-col :span="6">
+                      <a-input v-model="list.name" placeholder="請輸入" />
+                    </a-col>
+                    <div>商品條碼</div>
+                    <a-col :span="6">
+                      <a-input v-model="list.barCode" placeholder="請輸入" />
+                    </a-col>
+                    <div>單位</div>
+                    <a-select v-model="list.unit" style="width: 15%" placeholder="請選擇">
+                      <a-select-option value="斤">
+                        公斤
+                      </a-select-option>
+                      <a-select-option value="克">
+                        公克
+                      </a-select-option>
+                      <a-select-option value="件">
+                        件
+                      </a-select-option>
+                      <a-select-option value="台斤">
+                        台斤
+                      </a-select-option>
+                    </a-select>
+                  </div>
+                  <div class="secondPart">
+                    <div>售價</div>
+                    <a-col :span="4">
+                      <a-input v-model="list.prices" placeholder="請輸入" />
+                    </a-col>
+                    <div>建議售價</div>
+                    <a-col :span="4">
+                      <a-input v-model="list.sprices" placeholder="請輸入" />
+                    </a-col>
+                    <div>成本售價</div>
+                    <a-col :span="4">
+                      <a-input v-model="list.oprices" placeholder="請輸入" />
+                    </a-col>
+                  </div>
+                  <div class="thirdPart">
+                    <div>商品描述</div>
+                    <a-col :span="20">
+                      <a-input v-model="desc" style="height: 100px;" />
+                    </a-col>
+                  </div>
+                </div>
+              </a-row>
+            </a-input-group>
+          </div>
+          <template slot="footer">
+            <a-button key="submit" type="primary" :loading="loading" @click="handleOk">
+              儲存
+            </a-button>
+            <a-button key="back" @click="handleCancel">
+              取消
+            </a-button>
+          </template>
+        </a-modal>
+      </div>
+
+      <div class="search">
+        <a-input-search v-model="search" placeholder="搜尋內容" enter-button @click="onSearch" />
+      </div>
+    </div>
+    <div class="itemMenu">
+      <a-table :columns="columns" :data-source="filterText" bordered>
+        <template
+                v-for="col in ['order','barCode','name','unit','prices','sprices','oprices']"
+                :slot="col"
+                slot-scope="text, record, index"
+        >
+          <div :key="col">
+            <template v-if="col === 'order'">
+              {{index+1}}
+            </template>
+            <template>
+              {{ text }}
+            </template>
+          </div>
+        </template>
+        <template slot="operation" slot-scope="text, record">
+          <a-button size="small" @click="editHandler(record)">編輯</a-button>
+          <a-popconfirm
+                  title="確定要刪除嗎?"
+                  @confirm="() => onDelete(record.id)"
+          >
+            <a-button size="small">刪除</a-button>
+          </a-popconfirm>
+        </template>
+      </a-table>
+    </div>
   </div>
+
 </template>
-
 <script>
+  import axios from 'axios'
+
   export default {
-    
-  }
+    name:"Merchant",
+    data() {
+      return {
+        loading: false,
+        visible: false,
+        desc:"",
+        track:"",
+        search:"",
+        list:{
+          name:"",
+          unit:"",
+          barCode:"",
+          prices:"",
+          oprices:"",
+          sprices:"",
+        },
+        tableData:[],
+        changeTitle:"",
+        columns: [
+          {
+            title: '序',
+            dataIndex: 'order',
+            width: '2%',
+            align:"center",
+            scopedSlots: { customRender: 'order' },
+          },
+          {
+            title: '商品ID',
+            dataIndex: '',
+            width: '10%',
+            align:"center",
+            scopedSlots: { customRender: '' },
+          },
+          {
+            title: '商品條碼',
+            dataIndex: 'barCode',
+            width: '15%',
+            align:"center",
+            scopedSlots: { customRender: 'barCode' },
+          },
+          {
+            title: '商品名稱',
+            dataIndex: 'name',
+            width: '15%',
+            align:"center",
+            scopedSlots: { customRender: 'name' },
+          },
+          {
+            title: '單位',
+            dataIndex: 'unit',
+            width: '5%',
+            align:"center",
+            scopedSlots: { customRender: 'unit' },
+          },
+          {
+            title: '售價',
+            dataIndex: 'prices',
+            width: '10%',
+            align:"center",
+            scopedSlots: { customRender: 'prices' },
+          },
+          {
+            title: '建議售價',
+            dataIndex: 'sprices',
+            width: '10%',
+            align:"center",
+            scopedSlots: { customRender: 'sprices' },
+          },
+          {
+            title: '成本價',
+            dataIndex: 'oprices',
+            width: '10%',
+            align:"center",
+            scopedSlots: { customRender: 'oprices' },
+          },
+          {
+            title: '庫存量',
+            dataIndex: 'quantity',
+            width: '5%',
+            align:"center",
+            scopedSlots: { customRender: 'quantity' },
+          },
+          {
+            title: '建立時間',
+            dataIndex: 'createdTime',
+            width: '10%',
+            align:"center",
+            scopedSlots: { customRender: 'createdTime' },
+          },
+          {
+            title: 'operation',
+            dataIndex: 'operation',
+            width:'10%',
+            align:"center",
+            scopedSlots: { customRender: 'operation' },
+          },
+        ]
+      };
+    },
+    created() {
+      axios.get('http://localhost:3000/lists')
+              .then((res)=>{
+                console.log(res.data)
+                this.tableData = res.data
+              }).catch((err)=>{
+        console.log(err)
+      })
+    },
+    computed:{
+      filterText(){
+        if(!this.search){
+          return this.tableData
+        }else {
+          return this.tableData.filter(item=>{
+            return item.name.includes(this.search)
+          })
+        }
+      }
+    },
+    methods: {
+      showModal() {
+        this.visible = true;
+        this.changeTitle = "新增商品"
+      },
+      clearInput(){
+        this.list.name = "",
+                this.list.barCode = "",
+                this.list.prices = "",
+                this.list.sprices = "",
+                this.list.oprices =""
+      },
+      handleOk() {
+        this.loading = true;
+        if(this.changeTitle === "新增商品"){
+          axios.post('http://localhost:3000/lists',{
+            id:"",
+            name:this.list.name,
+            unit:this.list.unit,
+            barCode:this.list.barCode,
+            prices:this.list.prices,
+            sprices:this.list.sprices,
+            oprices:this.list.oprices
+          })
+                  .then(()=>{
+                    this.clearInput()
+                    axios.get('http://localhost:3000/lists')
+                            .then((res)=> {
+                              this.tableData = res.data
+                            })
+                  })
+          this.visible = false;
+          this.loading = false;
+        }else{
+          // if(this.changeTitle === "編輯商品")
+          axios.put('http://localhost:3000/lists/'+this.track,{
+            id:this.track,
+            name:this.list.name,
+            unit:this.list.unit,
+            barCode:this.list.barCode,
+            prices:this.list.prices,
+            sprices:this.list.sprices,
+            oprices:this.list.oprices
+          })
+                  .then(()=>{
+                    axios.get('http://localhost:3000/lists')
+                            .then((res)=>{
+                              this.tableData = res.data
+                            })
+                  })
+          this.visible = false;
+          this.loading = false;
+        }
+      },
+      handleCancel(e) {
+        this.visible = false;
+      },
+      editHandler(record){
+        this.track = record.id
+        this.changeTitle = "編輯商品"
+        axios.get('http://localhost:3000/lists/' + record.id)
+                .then((res)=>{
+                  this.list = res.data
+                  this.visible = true;
+                })
+      },
+      onDelete(id) {
+        axios.delete('http://localhost:3000/lists/'+ id)
+                .then(()=>{
+                  axios.get('http://localhost:3000/lists')
+                          .then((res)=>{
+                            this.tableData = res.data
+                          })
+                })
+      },
+      onSearch(){
+        if(this.search){
+          axios.get('http://localhost:3000/lists')
+                  .then((res)=>{
+                    this.tableData = res.data
+                  })
+        }
+      },
+      // getList(){
+      //     axios.get('http://localhost:3000/lists/'+this.search)
+      //     .then((res)=>{
+      //         this.tableData = res.data
+      //     }).catch((err)=>{
+      //         console.log(err)
+      //     })
+      // }
+    },
+  };
 </script>
+<style scoped lang="scss">
+  .button1{
+    background-color: #1ebf1e;
+    color: #fcfcfc;
+    font-weight: bold;
+    font-size: large;
+  }
+  .action{
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 15px;
+  }
+  .search{
+    width: 300px;
+  }
+  .modal-body{
+    background-color: #eee3e3;
 
-<style lang="scss" scoped>
+  }
+  .allPart{
+    margin: 25px;
 
+  }
+  .firstPart{
+    display: flex;
+    justify-content: space-between;
+  }
+  .secondPart{
+    margin: 25px 0px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .thirdPart{
+    display: flex;
+  }
 </style>
