5ca665427d8300b01174820922d6c8fe2e54daf7
LINK8\tris.xyh <tris.xyh@link8tech.tw>
2020-10-27 13:51:32 +0800

    【新增】loading功能 【修改】網站標題

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ import store from "./store";
 import 'ant-design-vue/dist/antd.css'
 import api from "./api/apis";
 import './style/main.scss'
+import loading from '@/plugins/Loading/index.js'
 
 Vue.config.productionTip = false;
 Vue.use(Antd)
+Vue.use(loading)
 Vue.prototype.$api = api;
 
 new Vue({
---
+++ src/plugins/Loading/Loading.vue
@@ -0,0 +1,34 @@
+<template>
+  <transition name="loading-fade">
+    <div v-show="visible" class="erp-loading">
+      <a-spin>
+        <a-icon slot="indicator" type="loading" style="font-size: 40px" spin />
+      </a-spin>
+    </div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    name: 'Loading',
+    data(){
+      return {
+        visible: false
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.erp-loading{
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(255,255,255,0.5);
+}
+</style>
