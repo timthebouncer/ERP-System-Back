0d228dd7eff5f210d7080ca58ec9e97036db2675
eric.wyj <eric.wyj@link8tech.tw>
2020-11-23 17:28:43 +0800

    更改 meun收合的ICON

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -4,26 +4,9 @@
       <a-icon
         class="trigger"
         align="flex-start"
-        :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'"
+        :type="$store.state.collapsed ? 'menu' : 'left'"
         @click="() => ($store.state.collapsed = !$store.state.collapsed)"
       />
-      <!--    <div class="logo"/>-->
-      <!--    <a-menu-->
-      <!--            theme="dark"-->
-      <!--            mode="horizontal"-->
-      <!--            :default-selected-keys="['2']"-->
-      <!--            :style="{ lineHeight: '64px' }"-->
-      <!--    >-->
-      <!--        <a-menu-item key="1">-->
-      <!--            nav 1-->
-      <!--        </a-menu-item>-->
-      <!--        <a-menu-item key="2">-->
-      <!--            nav 2-->
-      <!--        </a-menu-item>-->
-      <!--        <a-menu-item key="3">-->
-      <!--            nav 3-->
-      <!--        </a-menu-item>-->
-      <!--    </a-menu>-->
     </div>
     <div>
       登出
